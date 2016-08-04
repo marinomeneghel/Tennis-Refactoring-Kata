953807d4a58ab33204c7e7bae84f933d0ec4aa46
Marino Meneghel <marino.meneghel@xpeppers.com>
2016-08-04 14:30:50 +0200

    Removed for loop

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -34,48 +34,45 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
+        String scoreString = "";
         int tempScore=0;
         if(scoreEquals() && firstPlayer.getScore() >= 3) {
             return "Deuce";
         } else if (scoreEquals()) {
-            score = equalPointsMap.get(firstPlayer.getScore());
+            scoreString = equalPointsMap.get(firstPlayer.getScore());
         }
         else if (firstPlayer.getScore()>=4 || secondPlayer.getScore()>=4)
         {
             int minusResult = firstPlayer.getScore()-secondPlayer.getScore();
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) scoreString ="Advantage player1";
+            else if (minusResult ==-1) scoreString ="Advantage player2";
+            else if (minusResult>=2) scoreString = "Win for player1";
+            else scoreString ="Win for player2";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = firstPlayer.getScore();
-                else { score+="-"; tempScore = secondPlayer.getScore();}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        else {
+            scoreString = getScoreStringValue(firstPlayer);
+            scoreString += "-";
+            scoreString += getScoreStringValue(secondPlayer);
         }
-        return score;
+        return scoreString;
+    }
+
+    private String getScoreStringValue(Player player) {
+        switch(player.getScore()) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return null;
+        }
     }
 
     private boolean scoreEquals() {
-        return firstPlayer.getScore()==secondPlayer.getScore();
+        return firstPlayer.getScore() == secondPlayer.getScore();
     }
 }
