0efec451b8a69c0415283e9f15143f8317942e1b
Marino Meneghel <marino.meneghel@xpeppers.com>
2016-08-08 14:33:03 +0200

    Extracted getPlayerWithHigherScore and getIsWinOrAdvantage

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -14,27 +14,24 @@ class TennisGame1 implements TennisGame {
         getPlayerFromName(playerName).incrementScore();
     }
 
-    private Player getPlayerFromName(String name) {
-        return isFirstPlayer(name) ? firstPlayer : secondPlayer;
-    }
-
-    private boolean isFirstPlayer(String playerName) {
-        return firstPlayer.getName().equals(playerName);
-    }
-
     public String getScore() {
         if(playersScoreEquals()) {
             return firstPlayer.getScore() >= 3 ? "Deuce" : firstPlayer.getScoreString() + "-All";
 
         } else if (firstPlayer.getScore()>=4 || secondPlayer.getScore()>=4) {
-            int scoreDifference = getScoreDifference();
-            Player playerWithHigherScore = scoreDifference > 0 ? firstPlayer : secondPlayer;
-            String winOrAdvantage = Math.abs(scoreDifference) >= 2 ? "Win for " : "Advantage ";
-            return winOrAdvantage + playerWithHigherScore.getName();
+            return getIsWinOrAdvantage() + getPlayerWithHigherScore().getName();
 
         } else {
             return firstPlayer.getScoreString() + "-" + secondPlayer.getScoreString();
         }
+    }
+
+    private String getIsWinOrAdvantage() {
+        return Math.abs(getScoreDifference()) >= 2 ? "Win for " : "Advantage ";
+    }
+
+    private Player getPlayerWithHigherScore() {
+        return getScoreDifference() > 0 ? firstPlayer : secondPlayer;
     }
 
     private int getScoreDifference() {
@@ -43,5 +40,13 @@ class TennisGame1 implements TennisGame {
 
     private boolean playersScoreEquals() {
         return firstPlayer.doHaveSameScoreOf(secondPlayer);
+    }
+
+    private Player getPlayerFromName(String name) {
+        return isFirstPlayer(name) ? firstPlayer : secondPlayer;
+    }
+
+    private boolean isFirstPlayer(String playerName) {
+        return firstPlayer.getName().equals(playerName);
     }
 }
